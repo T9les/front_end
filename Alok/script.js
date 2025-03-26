879393f9319d2ed0097361d15ba5bf2e2df2ecd9
Tales Alves <136338214+T9les@users.noreply.github.com>
2025-03-25 21:36:58 -0300

    Update script.js

--- Alok/script.js
+++ Alok/script.js
@@ -1,1 +1,37 @@
+// console.log("var variavel");
+// console.log(x);
+// var x = 5;
+// console.log(x);
 
+// console.log("let variavel");
+// console.log(y);
+// let y = 6;
+// console.log(y); 
+
+// console.log("const variavel");
+// console.log(z);
+// const z = 7;
+// console.log(z);
+// console.log(z);
+
+function main(){
+    console.log("var variavel");
+    console.log(x);
+    var x = 5;
+    console.log(x);
+    
+    console.log("let variavel");
+    console.log(y);
+    let y = 6;
+    console.log(y); 
+    
+    console.log("const variavel");
+    console.log(z);
+    // const z = 7;
+    console.log(z);
+    console.log(z);  
+}
+
+const z = 7;
+main();
+console.log(z);
